5cd4c5c31dd1a9543cc2e524f8a9b2accdd7d45c
blexcel <obichioma123@yahoo.com>
2022-06-22 10:42:04 +0100

    Print My name

---
+++ new.js
@@ -0,0 +1,7 @@
+const myName = "Obi Chioma Blessing";
+const height = "5.9";
+const country = "Nigeria";
+  
+console.log("My name is: ", myName + "\n");
+console.log("My height is: ", height + "\n");
+console.log("I am from: ", country);
