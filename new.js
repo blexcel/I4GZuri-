0a297fb79603ed9c8a7bd548e6a6651ac1979ec8
blexcel <obichioma123@yahoo.com>
2022-06-22 14:00:04 +0100

    My details

--- new.js
+++ new.js
@@ -5,3 +5,5 @@ const country = "Nigeria";
 console.log("My name is: ", myName + "\n");
 console.log("My height is: ", height + "\n");
 console.log("I am from: ", country);
+
+alert("My name is: " + myName + ", My height is: " + height + " and I am from " + country)
